39d6f0e98bcac9202387b6d689465c39b0dab8b8
gauripatil-2609 <95200437+gauripatil-2609@users.noreply.github.com>
2021-11-29 11:05:36 +0530

    Scenario B passed

--- src/Cart.java
+++ src/Cart.java
@@ -35,16 +35,53 @@ public class Cart {
 		
 		for(String item: cart_items.keySet()) {
 			
-			TotalCost = TotalCost + (cart_items.get(item)*inventory.get(item));
+			TotalCost = TotalCost + ApplyPromotion(item,cart_items.get(item));
 		}
 		
 		return TotalCost;
 	}
+	
+    int ApplyPromotion(String item , int units) {
+    	int Total =0;
+    	int number_of_C =0;
+    	switch(item) {
+    	case "A" :
+    		Total =((units/3)*130 + (units%3)*50);
+    		break;
+    	case "B" :
+    		Total = ((units/2)*45 + (units%2)*30);
+    		break;
+    	case "C" :
+    		number_of_C = units;
+    		Total = units*20;
+    		break;
+    	case "D" :
+    		if (number_of_C == 0) {
+    			Total = units*15;
+    		}
+    		else {
+    			if (number_of_C == units) {
+    				Total = units*30;
+    			}
+    			else if (number_of_C < units) {
+    				Total = (units - number_of_C)*15 + number_of_C *30;
+    				
+    			}
+    			else {
+    				Total = (number_of_C - units)*20 + units * 30;
+    			}
+    		}
+    		break;
+    		
+    	}
+    	return Total;
+    }
 	public void add_item(String string, int i) {
 		// TODO Auto-generated method stub
 		cart_items.put(string,i);
 				
 	}
+	
 	
 
 }
