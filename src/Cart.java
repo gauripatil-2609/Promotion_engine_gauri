97b8144f655fd1a5e1ac0307cfed8e227777faf0
gauripatil-2609 <95200437+gauripatil-2609@users.noreply.github.com>
2021-11-29 10:15:50 +0530

    First Test Passed

--- src/Cart.java
+++ src/Cart.java
@@ -4,12 +4,20 @@ import java.lang.*;
 public class Cart {
 	
 	Map<String, Integer> inventory = new HashMap<String, Integer>();
-	Map<String, Integer> cost_sheet = new HashMap<String, Integer>();
+	Map<String, Integer> cart_items = new HashMap<String, Integer>();
+	
+	
+	
 	public double getTotal() {
-		int TotalCost =-1;
+		double TotalCost =-1;
+		
+		if (cart_items.isEmpty()) {
+			TotalCost = 0.0;
+		}
 		
 		return TotalCost;
 		
 	}
+	
 
 }
