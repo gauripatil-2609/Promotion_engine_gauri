62308779c2439a2e4b795548a1b284e9b336955a
gauripatil-2609 <95200437+gauripatil-2609@users.noreply.github.com>
2021-11-29 10:29:49 +0530

    ScenarioA test

--- src/Cart.java
+++ src/Cart.java
@@ -7,16 +7,25 @@ public class Cart {
 	Map<String, Integer> cart_items = new HashMap<String, Integer>();
 	
 	
-	
-	public double getTotal() {
-		double TotalCost =-1;
+	public int getTotal() {
+		int TotalCost =-1;
 		
 		if (cart_items.isEmpty()) {
-			TotalCost = 0.0;
+			TotalCost = 0;
 		}
-		
+		else {
+			
+		}
 		return TotalCost;
 		
+	}
+
+
+
+	public void add_item(String string, int i) {
+		// TODO Auto-generated method stub
+		cart_items.put(string,i);
+				
 	}
 	
 
--- src/PromotionEngineTest.java
+++ src/PromotionEngineTest.java
@@ -4,12 +4,23 @@ import static org.junit.Assert.assertEquals;
 
 public class PromotionEngineTest {
 	
-	Cart shoppingCart = new Cart();
+	
 	@Test
 	
 	public void TotalofEmptyCart() {
+		Cart shoppingCart = new Cart();
+		assertEquals(0,shoppingCart.getTotal(),0); 
 		
-		assertEquals(0.0,shoppingCart.getTotal(),0.0); 
+	}
+	
+	@Test
+	
+	public void TestScenarioA() {
+		Cart shoppingCart = new Cart();
+		shoppingCart.add_item("A",1);
+		shoppingCart.add_item("B",1);
+		shoppingCart.add_item("C",1);
+		assertEquals(100,shoppingCart.getTotal(),0);
 		
 	}
 
